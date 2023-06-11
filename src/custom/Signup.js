1b8277724f00429b1b3e82043fc53a413b2712bb
Sarvesh E B <sarveshebs@gmail.com>
2023-06-12 00:43:11 +0530

    added email validation, changed size

--- src/custom/Signup.js
+++ src/custom/Signup.js
@@ -21,15 +21,65 @@ import {
   Container,
   Row,
   Col,
+  UncontrolledTooltip,
 } from "reactstrap";
-
+import IndexNavbar from "components/Navbars/IndexNavbar";
+import PageHeader from "components/PageHeader/PageHeader";
+import { useNavigate } from "react-router-dom";
+import Footer from "components/Footer/Footer";
+let name,email,password;
 export default function Signup() {
   const [fullNameFocus, setFullNameFocus] = React.useState(false);
   const [emailFocus, setEmailFocus] = React.useState(false);
+  const [emailError, setEmailError] = React.useState(false);
   const [passwordFocus, setPasswordFocus] = React.useState(false);
+  let navigate = useNavigate();
+
+  React.useEffect(() => {
+    document.body.classList.toggle("index-page");
+    // Specify how to clean up after this effect:
+    return function cleanup() {
+      document.body.classList.toggle("index-page");
+    };
+  }, []);
+  function handleEmail(e){
+    setEmailFocus(false);
+
+    let emailText=e.target.value;
+    const re=/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+    if(!re.test(emailText))
+      setEmailError(true)
+    else
+      setEmailError(false)
+  
+  }
+  function handleRegister(){
+    if(!emailError)
+    {
+      console.log(name,email,password)
+      if(password&&email&&name)
+      {
+        //TODO add code here to send request to backend with the registration details
+        navigate("/home")
+      }
+    }
+    
+
+  }
+
   return (
+    <div className="wrapper">
+
+    <div className="main">
+
     <div className="section section-signup">
+      <IndexNavbar />
       <Container>
+      <div className="squares square-1" />
+        <div className="squares square-2" />
+        <div className="squares square-3" />
+        <div className="squares square-4" />
         <Row className="row-grid justify-content-between align-items-center">
           <Col lg="5">
             <h3 className="display-3 text-white">
@@ -63,16 +113,25 @@ export default function Signup() {
                       </InputGroupText>
                     </InputGroupAddon>
                     <Input
+                      style={{"font-size": "18px","height": "50px"}}
                       placeholder="Full Name"
                       type="text"
                       onFocus={(e) => setFullNameFocus(true)}
                       onBlur={(e) => setFullNameFocus(false)}
+                      onChange={(e)=>name=e.target.value}
                     />
+                    
                   </InputGroup>
+                 {/* <div className="has-danger">
+                  <Input
+                  className=" has-danger form-control-danger"
+                  type="email"
+                />
+                </div> */}
                   <InputGroup
-                    className={classnames({
+                    className={!emailError?classnames({
                       "input-group-focus": emailFocus,
-                    })}
+                    }):"has-danger"}
                   >
                     <InputGroupAddon addonType="prepend">
                       <InputGroupText>
@@ -80,10 +139,12 @@ export default function Signup() {
                       </InputGroupText>
                     </InputGroupAddon>
                     <Input
+                      style={{"font-size": "18px","height": "50px"}}
                       placeholder="Email"
-                      type="text"
+                      type="email"
                       onFocus={(e) => setEmailFocus(true)}
-                      onBlur={(e) => setEmailFocus(false)}
+                      onBlur={handleEmail}
+                      onChange={(e)=>email=e.target.value}
                     />
                   </InputGroup>
                   <InputGroup
@@ -97,11 +158,21 @@ export default function Signup() {
                       </InputGroupText>
                     </InputGroupAddon>
                     <Input
+                      id="tooltip1"
+                      style={{"font-size": "18px","height": "50px"}}
                       placeholder="Password"
                       type="text"
                       onFocus={(e) => setPasswordFocus(true)}
                       onBlur={(e) => setPasswordFocus(false)}
+                      onChange={(e)=>password=e.target.value}
                     />
+                    <UncontrolledTooltip
+                      delay={0}
+                      placement="bottom"
+                      target="tooltip1"
+                    >
+                      Password should be alphanumeric
+                    </UncontrolledTooltip>
                   </InputGroup>
                   <FormGroup check className="text-left">
                     <Label check>
@@ -116,7 +187,10 @@ export default function Signup() {
                 </Form>
               </CardBody>
               <CardFooter>
-                <Button className="btn-round" color="primary" size="lg">
+                <Button 
+                  className="btn-round" color="primary" size="lg"
+                  onClick={handleRegister}
+                >
                   Get Started
                 </Button>
               </CardFooter>
@@ -124,6 +198,9 @@ export default function Signup() {
           </Col>
         </Row>
       </Container>
+      </div>
+    <Footer />
+  </div>
     </div>
   );
 }
