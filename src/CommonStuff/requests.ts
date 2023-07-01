47fbb71ba307a200f7762b7df3be4cd45207c1a1
Muthu <muthukumaran.u@ronianalytics.com>
2023-07-01 19:35:03 +0530

    httpget push

--- src/CommonStuff/requests.ts
+++ src/CommonStuff/requests.ts
@@ -1,7 +1,8 @@
 import cookie from 'react-cookie'
+import { formatDiagnostic } from 'typescript';
 
 export const BASE_URL = "http://localhost:8000"
-export function HttpPOST(base_url:string, endpoint: string, payload:Object,include_cookie:boolean=false){
+export function HttpPOST(base_url:string, endpoint: string, payload:Object,include_cookie:boolean=true){
     let formdata = new FormData();
 
     Object.keys(payload).forEach(key => formdata.append(key, payload[key]));
@@ -18,3 +19,25 @@ export function HttpPOST(base_url:string, endpoint: string, payload:Object,inclu
       })
       .catch((err)=>{return Promise.reject({"message":"Network request issue"})});
     }
+
+
+
+export function HttpGET(base_url:string, endpoint: string, payload: Object, include_cookie:boolean=true){
+
+  let endpoint_extension = endpoint + '?'
+
+  Object.keys(payload).forEach(key => endpoint_extension + key+'='+ payload[key]);
+
+  
+
+  return fetch(`${base_url}/${endpoint_extension}/`, {
+      method: 'GET',
+      credentials: include_cookie?'include':'same-origin'
+  }
+  ).then(response => {
+      return response.json().then(json => {
+        return response.ok ? json : Promise.reject(json);
+      });
+    })
+    .catch((err)=>{return Promise.reject({"message":"Network request issue"})});
+  }
